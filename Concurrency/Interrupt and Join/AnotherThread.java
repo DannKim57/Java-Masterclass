66a254d55201cf714b40f77808c644faef44d0a1
DannKim57 <51385906+DannKim57@users.noreply.github.com>
2020-01-07 16:09:00 +0900

    Update AnotherThread.java

--- Concurrency/Interrupt and Join/AnotherThread.java
+++ Concurrency/Interrupt and Join/AnotherThread.java
@@ -1,10 +1,6 @@
-package com.timbuchalka;
 
 import static com.timbuchalka.ThreadColor.ANSI_BLUE;
 
-/**
- * Created by timbuchalka on 25/05/2016.
- */
 public class AnotherThread extends Thread {
 
     @Override
