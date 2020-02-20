5da3872cd8a7e8fa830f3a951fd7530392a2ea9f
DannKim57 <51385906+DannKim57@users.noreply.github.com>
2020-02-20 15:05:07 +0900

    Create Main.java

---
+++ report-program/Main.java
@@ -0,0 +1,18 @@
+package report-program;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+// Report Program.
+// Each employee can write reports which are going to be given to employers.
+// Each employee only can read their own reports. While, employers can read all reports. 
+// I am going to use linked list as stack to sort reports in recently sent order, 
+// and queue to send reports in sent order.
+// In addition, I am going to use hashmap to map each employee to each key. 
+
+
